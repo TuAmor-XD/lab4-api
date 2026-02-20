9bd3bdda5cc90a4d0ce66180d95145b0045d32e9
TuAmor-XD <2021256010@ub.edu.bz>
2026-02-20 09:32:58 -0600

    Completed Part 2: The Custom Response Writer

--- main.go
+++ main.go
@@ -8,3 +8,14 @@ import (
 	"os"
 	"time"
 )
+
+type responseWriter struct {
+	http.ResponseWriter// embed the real writer
+	statusCode int // captured status code
+}
+
+// WriteHeader intercepts the status code before forwarding it.
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
